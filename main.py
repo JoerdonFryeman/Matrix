43191e815f7e610e9f35fc711c86d91a994887fc
Kepler <keplerpocket@proton.me>
2023-10-11 23:44:21 +0200

    4. The Neo class has been added

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import bext
+from neo import Neo
 from time import sleep
 from threading import Thread
 from matrix_for_windows import MatrixForWindows
 
+neo = Neo()
 matrix_windows = MatrixForWindows()
 
 
 def main() -> None:
     matrix_windows.get_title()
+    neo.run_text()
     matrix_windows.get_full_screen()
     sleep(0.1)
     matrix_windows.get_matrix_move(-1, bext.height() - 2)
---
+++ neo.py
@@ -0,0 +1,45 @@
+from os import system
+from time import sleep
+from random import randint
+
+
+class Neo:
+    wake_up = (
+        'W', 'Wa', 'Wak', 'Wake', 'Wake ', 'Wake u', 'Wake up', 'Wake up,', 'Wake up, ', 'Wake up, N',
+        'Wake up, Ne', 'Wake up, Neo', 'Wake up, Neo.', 'Wake up, Neo..', 'Wake up, Neo...'
+    )
+
+    the_matrix = (
+        'T', 'Th', 'The', 'The ', 'The M', 'The Ma', 'The Mat', 'The Matr', 'The Matri', 'The Matrix', 'The Matrix ',
+        'The Matrix h', 'The Matrix ha', 'The Matrix has', 'The Matrix has ', 'The Matrix has y', 'The Matrix has yo',
+        'The Matrix has you', 'The Matrix has you.', 'The Matrix has you..', 'The Matrix has you...'
+    )
+
+    follow = (
+        'F', 'Fo', 'Fol', 'Foll', 'Follo', 'Follow', 'Follow ', 'Follow t', 'Follow th', 'Follow the', 'Follow the ',
+        'Follow the w', 'Follow the wh', 'Follow the whi', 'Follow the whit', 'Follow the white', 'Follow the white ',
+        'Follow the white r', 'Follow the white ra', 'Follow the white rab', 'Follow the white rabb',
+        'Follow the white rabbi', 'Follow the white rabbit', 'Follow the white rabbit.'
+    )
+
+    @staticmethod
+    def generate_text(word, pause_first, pause_second, speed_first, speed_second):
+        count = 0
+        system('cls')
+
+        for i in range(len(word)):
+            count += 1
+            sleep(float(f'0.{randint(speed_first, speed_second)}'))
+            print(word[count - 1])
+
+        sleep(pause_first)
+        print(24 * ' ')
+        sleep(pause_second)
+
+    def run_text(self):
+        self.generate_text(self.wake_up, 4.4, 1.2, 1, 2)
+        self.generate_text(self.the_matrix, 4.2, 0.7, 2, 4)
+        self.generate_text(self.follow, 4, 0.2, 1, 3)
+        print('Knock, knock, Neo.')
+        sleep(4.2)
+        system('cls')

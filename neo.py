4c3208e0d79940ffdcc5cd290bad9df37c5a31b3
Kepler <keplerpocket@proton.me>
2024-06-29 16:45:15 +0200

    21. The code for the neo module has been completely changed

--- neo.py
+++ neo.py
@@ -1,63 +1,43 @@
-from os import system
-from bext import goto
+import os
 from time import sleep
-from colorama import Fore
 from random import randint
+from platform import system
+from colorama import Fore
 
 
 class Neo:
-    wake_up = (
-        'W', 'Wa', 'Wak', 'Wake', 'Wake ', 'Wake u', 'Wake up', 'Wake up,', 'Wake up, ', 'Wake up, N',
-        'Wake up, Ne', 'Wake up, Neo', 'Wake up, Neo.', 'Wake up, Neo..', 'Wake up, Neo...'
-    )
-
-    the_matrix = (
-        'T', 'Th', 'The', 'The ', 'The M', 'The Ma', 'The Mat', 'The Matr', 'The Matri', 'The Matrix', 'The Matrix ',
-        'The Matrix h', 'The Matrix ha', 'The Matrix has', 'The Matrix has ', 'The Matrix has y', 'The Matrix has yo',
-        'The Matrix has you', 'The Matrix has you.', 'The Matrix has you..', 'The Matrix has you...'
-    )
-
-    follow = (
-        'F', 'Fo', 'Fol', 'Foll', 'Follo', 'Follow', 'Follow ', 'Follow t', 'Follow th', 'Follow the', 'Follow the ',
-        'Follow the w', 'Follow the wh', 'Follow the whi', 'Follow the whit', 'Follow the white', 'Follow the white ',
-        'Follow the white r', 'Follow the white ra', 'Follow the white rab', 'Follow the white rabb',
-        'Follow the white rabbi', 'Follow the white rabbit', 'Follow the white rabbit.'
-    )
-
     @staticmethod
-    def generate_text(word, pause_first, pause_second, speed_first, speed_second) -> None:
-        """
-        The function takes a tuple of words and prints them at a certain speed
-        :param word: str
-        :param pause_first: float
-        :param pause_second: float
-        :param speed_first: int
-        :param speed_second: int
-        :return: None
-        """
-        count = 0
-        system('cls')
+    def get_system_command() -> str:
+        system_name = system()
+        if system_name == 'Linux':
+            return 'clear'
+        elif system_name == 'Windows':
+            return 'cls'
 
-        for i in range(len(word)):
-            count += 1
-            sleep(float(f'0.{randint(int(speed_first), int(speed_second))}'))
-            goto(4, 2)
-            print(f'{Fore.GREEN}{word[count - 1]}')
-
-        sleep(float(pause_first))
-        goto(4, 2)
-        print(24 * ' ')
-        sleep(float(pause_second))
-
-    def run_text(self) -> None:
+    def wake_up_neo(self, sentences_list: list):
         """
-        The function calls the text generating function
-        :return: None
+        The function takes a list of words and return as a printed input
+        :param sentences_list: list
         """
-        self.generate_text(self.wake_up, 4.4, 1.2, 1, 2)
-        self.generate_text(self.the_matrix, 4.2, 0.7, 2, 4)
-        self.generate_text(self.follow, 4, 0.2, 1, 3)
-        goto(4, 2)
+        counter_first = 0
+        for text in sentences_list:
+            counter_first += 1
+            counter_second = 0
+            sentence = [i for i in str(text)]
+            for i in range(len(sentence)):
+                counter_second += 1
+                if counter_first == 1:
+                    sleep(float(f'0.{randint(1, 3)}'))
+                elif counter_first == 2:
+                    sleep(float(f'0.{randint(2, 4)}'))
+                elif counter_first == 3:
+                    sleep(float(f'0.{randint(1, 3)}'))
+                else:
+                    sleep(float(f'0.{randint(randint(1, 2), randint(3, 4))}'))
+                os.system(self.get_system_command())
+                print(f'{Fore.GREEN}'.join(sentence[0:counter_second]))
+            sleep(float(4))
+        os.system(self.get_system_command())
         print(f'{Fore.GREEN}Knock, knock, Neo.')
         sleep(4.2)
-        system('cls')
+        os.system(self.get_system_command())

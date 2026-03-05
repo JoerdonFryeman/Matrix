1f744f43e8740c3bb8000730fa71e5288ca4423a
Joerdon Fryeman <JoerdonFryeman@proton.me>
2026-03-05 21:11:59 +0100

    81. В метод paint добавлены доки.

--- core/visualisation.py
+++ core/visualisation.py
@@ -66,6 +66,7 @@ class Visualisation(Base):
         return choice(pools)
 
     def paint(self, rainbow: int | None, color: str, a_bold: bool) -> int:
+        """Раскрашивает текст или текстовое изображение."""
         colors = ('MAGENTA', 'BLUE', 'CYAN', 'GREEN', 'YELLOW', 'RED', 'WHITE', 'BLACK')
         for i in range(1, len(colors) + 1):
             init_pair(i, self.verify_color(colors[i - 1]), -1)
